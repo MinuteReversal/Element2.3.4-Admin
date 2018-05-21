7cae52a174ca0e37a128f6592cd3b8a69c5dcbcf
MinuteReversal <114233763@qq.com>
2018-05-21 18:09:24 +0800

    name

---
+++ plugins/v-name.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+
+Vue.directive("name", {
+    // 当被绑定的元素插入到 DOM 中时……
+    inserted: function (el) {
+      // 聚焦元素
+      el.focus();
+      window.$nuxt.$store.state.modules.buttonnames;
+    }
+  })
